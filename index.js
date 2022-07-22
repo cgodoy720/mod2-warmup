4dd7dd89efc797d92c6c4bba7ba1c565fe642780
Carlos Godoy <carlosgodoy@pursuit.org>
2022-07-22 12:15:48 -0400

    added class notes

--- index.js
+++ index.js
@@ -5,21 +5,53 @@
 //     console.log('Get ready for Module 2!')
 // }, 2000)
 
+// console.log('I fire first')
 
+function logToConsole(){
+    console.log('I fire every two seconds!')
+
+}
 
 // setInterval
-// setInterval(() => {
-//     console.log('I fire every two seconds!')
-// }, 2000)
+// setInterval(logToConsole, 2000)
 
 
 
 const modules = ["Fundamentals", "FEWD", "React", "Full Stack Web Dev", "Data Structures & Algorithms", "Capstone Project"]
 
 
+
+// Will do something for each element in the array, whether you modify it or not
 // forEach()
+modules.forEach((mod) => {
+    console.log(mod)
+})
+
+// Shorter syntax
+// - no need for parens when one argument in callback 
+// - no need for curly brackets when function is short enough to fit on one line
+// - return is implied when we do not have curly brackets
+// modules.forEach(mod => console.log(mod))
 
 
-
+// Creates a new modified array, without modifying the original array
 // map()
+// const newModules = modules.map( (mod, i) => {
+//     return `Module ${i + 1} ${mod}`
+// })
 
+// Will throw error bc newModules is a const
+// newModules = modules.map( (mod, i) => {
+//     return `Module ${i + 1} ${mod}`
+// } )
+
+
+
+// console.log(newModules)
+
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    // do something when button is clicked
+})
